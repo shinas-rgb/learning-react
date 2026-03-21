9a19edddf02ab504f8c07b973a13190354886ebc
shinas-rgb <shinasmuhammedkt@gmail.com>
2026-03-21 17:58:41 +0530

    prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
 
+function Greeting(props) {
+  return <h1>Hello Iam {props.name} {props.age} years old</h1>
+}
+
 function App() {
 
   return (
     <div>
-      <h1>Hello world</h1>
+      <Greeting name={"Shinas"} age={18} />
+      <Greeting name={"Jessica"} age={22} />
+      <Greeting name={"Jack"} age={25} />
+      <Greeting name={"Micheal"} age={19} />
     </div>
   )
 }
