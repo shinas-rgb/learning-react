a77534399d4d42dfada687fd3df1652db91bce0c
shinas-rgb <shinasmuhammedkt@gmail.com>
2026-03-21 18:41:30 +0530

    state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
 import './App.css'
+import { useState } from 'react';
 
 function Greeting(props) {
   return <h1>Hello Iam {props.name} {props.age} years old</h1>
 }
 
 function App() {
+  const [showGreeting, setShowGreeting] = useState(false);
+  const [name, setName] = useState("");
 
+  function toggleGreeting() {
+    setShowGreeting(!showGreeting);
+  }
+
+  function handleChange(event) {
+    const value = event.target.value;
+    setName(value);
+  }
   return (
     <div>
-      <Greeting name={"Shinas"} age={18} />
-      <Greeting name={"Jessica"} age={22} />
-      <Greeting name={"Jack"} age={25} />
-      <Greeting name={"Micheal"} age={19} />
+      <button onClick={toggleGreeting}>Click Me</button>
+      {showGreeting && (
+        <Greeting name={"malik"} age={32} />
+      )}
+
+      <br />
+      <br />
+      <input type="text" placeholder="Name" onChange={handleChange} />
+      <br />
+      {name}
+      {/* {showGreeting ? ( */}
+      {/*   <Greeting name={"Shinas"} age={18} /> */}
+      {/* ) : ( */}
+      {/*   <button>Click me</button> */}
+      {/* )} */}
     </div>
   )
 }
